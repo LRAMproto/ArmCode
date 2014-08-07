d33c237131d95e1524767bb6cc7c7feba83de9cd
rebhuhnd <rebhuhnd@onid.orst.edu>
2014-08-07 01:36:21 -0700

    StateChangeEventData improvements.
    Modified state change event data for better documentation.

--- Caster FSM/+FSM/StateChangeEventData.m
+++ Caster FSM/+FSM/StateChangeEventData.m
@@ -1,20 +1,23 @@
 classdef StateChangeEventData < event.EventData
     % FSM.StateChangeEventData
-    % Event Data for the given state machine.
+    % Eventdata structure for use in interpreting StateChange events of
+    % objects of type FSM.StateMachine.
     
     properties
         stateMachine
-        % The source of the event.
+        % The source of the event. (FSM.StateMachine)
         currentState
-        % The current state.
+        % The current state. (FSM.State)
         input
-        % The last state.
+        % The last input. (char)
         lastState
+        % The previous state (FSM.State)
     end
     
     methods
         function obj = StateChangeEventData(...
                 stateMachine, currentState, input, lastState)
+            % Defines the eventdata structure.
             obj.stateMachine = stateMachine;
             obj.currentState = currentState;
             obj.input = input;
