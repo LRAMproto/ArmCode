e8a682f0414282544c5aa308d95d4dc85c365180
rebhuhnd <rebhuhnd@onid.orst.edu>
2014-08-07 01:35:29 -0700

    State improvements.
    Modified documentation of state. Made state a handle object for future
    state definition use.

--- Caster FSM/+FSM/State.m
+++ Caster FSM/+FSM/State.m
@@ -1,12 +1,16 @@
-classdef State
+classdef State < hgsetget
     % Current state of a state machine.
     properties
         id
+        % Identification of a given state (int32).
         name
+        % Name of a given state (char).
     end
     
     methods
         function obj = State(id, name)
+            % Initializes the state. Note that names do not have to be
+            % defined.
             obj.id = int32(id);
             if nargin > 1
                 obj.name = name;
@@ -14,6 +18,7 @@ classdef State
         end
         
         function disp(obj)
+            % Custom Display Function.
             fprintf('State: %d (%s)\n',obj.id, obj.name);
         end
         
